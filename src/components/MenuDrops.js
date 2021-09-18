80d2a61a91002d50fe36f88e5c06c7e076061795
Vanessa Carlos <vanessaboldrim16@gmail.com>
2021-09-17 23:08:08 -0300

    alt img

--- src/components/MenuDrops.js
+++ src/components/MenuDrops.js
@@ -15,11 +15,11 @@ function MenuDrop (){
                     <li className={styles.menu_branco}>
                         <i className={styles.material_icons}><CgPokemon /></i>
                             <ul>
-                                <li><div className={styles.fotoMenu}><img className={styles.devfoto} src={MinhaFoto}/><p>Seja bem vindo Treinador</p><br/><p>Obrigado pela visita!</p></div></li>
-                                <li><a href="https://www.facebook.com/Hinaliboldrim/"><img className={styles.icons}src={FaceIcon}/>FaceBook</a></li>
-                                <li><a href="https://www.instagram.com/hinaliboldrim/"><img className={styles.icons} src={InstaIcon}/>Instagram</a></li>
-                                <li><a href="https://www.linkedin.com/in/vanessa-carlos-277973206/"><img className={styles.icons} src={LinkIcon}/>Linkedin</a></li>
-                                <li><a href="https://github.com/hinali16"><img className={styles.icons} src={GitIcon}/>Github</a></li>
+                                <li><div className={styles.fotoMenu}><img className={styles.devfoto} src={MinhaFoto} alt="devimg"/><p>Seja bem vindo Treinador</p><br/><p>Obrigado pela visita!</p></div></li>
+                                <li><a href="https://www.facebook.com/Hinaliboldrim/"><img className={styles.icons}src={FaceIcon} alt="faceimg"/>FaceBook</a></li>
+                                <li><a href="https://www.instagram.com/hinaliboldrim/"><img className={styles.icons} src={InstaIcon} alt="instaimg"/>Instagram</a></li>
+                                <li><a href="https://www.linkedin.com/in/vanessa-carlos-277973206/"><img className={styles.icons} src={LinkIcon} alt="linkimg"/>Linkedin</a></li>
+                                <li><a href="https://github.com/hinali16"><img className={styles.icons} src={GitIcon} alt="gitimg"/>Github</a></li>
                                 <li><a href="https://pokeapi.co/">Link da API utilizada</a></li>
                             </ul>
                     </li>
