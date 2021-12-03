87eb84b8cf2f76f4a1a063f35b089e3650602815
Vanessa Carlos <vanessaboldrim16@gmail.com>
2021-12-03 05:02:06 -0300

    correcao estilo

--- src/components/PesquisaPokemon/index.js
+++ src/components/PesquisaPokemon/index.js
@@ -10,38 +10,43 @@ export default function PesquisaPokemon(){
     const [pesquisando, setPesquisando] = useState('');
     const [pokemon, setPokemon] = useState();
     const [loading, setLoading] = useState(true);
-    const [pesoPokemon, setPesoPokemon] = useState('');
-    console.log(pesoPokemon)
 
  
 
     function buscarPokemon(){
-        const req = axios.get(`https://pokeapi.co/api/v2/pokemon/${pesquisando.toLocaleLowerCase()}`)
-        req.then(({data})=>{
+      if(pesquisando != ''){
+      const req = axios.get(`https://pokeapi.co/api/v2/pokemon/${pesquisando.toLocaleLowerCase()}`)
+      req.then(({data})=>{
             setPokemon(data);
             setLoading(false);
-            setPesoPokemon(data.weight);
-            console.log(setPesoPokemon)
-        })
-
+        });
+      }
     }
-    let pesoComVirgula = parseInt(pesoPokemon + '0');
+
+
+    let pesoComVirgula = parseInt(pokemon?.weight + '0');
     function separator(numb) {
       var str = numb.toString().split(".");
       str[0] = str[0].replace(/\B(?=(\d{2})(?!\d)+)/g, ".");
       return str.join(".");
     }
+      const apertaButao = (e) => {
+        if (e.key === 'Enter') {
+          buscarPokemon();
+        }
+      };
     console.log(separator(pesoComVirgula));
     return (
       <>
         <Logo src={logo} alt="pokedexImg" />
 
-        {loading ? (<>
+        {loading ? (
+          <>
             <PokedexContainerImg>
-                <ImgPokebola src={pokebola} alt="pokebolaImg" />
+              <ImgPokebola src={pokebola} alt="pokebolaImg" />
             </PokedexContainerImg>
             <Paragrafo>Digite um nome ou numero do pokemon!</Paragrafo>
-            </>
+          </>
         ) : (
           <CardPokemon>
             <CardPokemonFoto>
@@ -49,31 +54,55 @@ export default function PesquisaPokemon(){
                 <p>{pokemon.name}</p>
                 <span>#{pokemon.id}</span>
               </NomeID>
-              <img src={pokemon.sprites.front_default} alt="imagemPokemon"/>
+              <PokemonFoto src={pokemon.sprites.front_default} alt="imagemPokemon" />
               <PesoAltura>
-                <p>{separator(pesoComVirgula)}Kg</p>
-                <p>Altura{pokemon.height}</p>
+                <p>Peso: {separator(pesoComVirgula)}Kg <Linha /></p>
+                <p>Altura: {pokemon.height} <Linha /></p>
               </PesoAltura>
-              
             </CardPokemonFoto>
             <CardPokemonDetalhes>
-              {pokemon.types[0].type.name}
-              HP{pokemon.stats[0].base_stat}
-              Attack{pokemon.stats[1].base_stat}
-              Defense{pokemon.stats[2].base_stat}
-              Special-Attack{pokemon.stats[3].base_stat}
-              Special-Defense{pokemon.stats[4].base_stat}
-              Speed{pokemon.stats[5].base_stat}
-
-          
+              <TipoPokemon>
+                {pokemon.types.map((x,index) => {   
+                  return <Colunas><p>{x.type.name}</p></Colunas>})}
+              </TipoPokemon>
+              <ContainerColunas>
+                <Colunas>
+                  <span>
+                    HP: {pokemon.stats[0].base_stat}
+                  </span>
+                  <Linha />
+                  <span>
+                    Attack: {pokemon.stats[1].base_stat}
+                  </span>
+                  <Linha />
+                  <span>
+                    Defense: {pokemon.stats[2].base_stat}
+                  </span>
+                  <Linha />
+                </Colunas>
+                <Colunas>
+                  <span>
+                    Sp.Atk: {pokemon.stats[3].base_stat}
+                  </span>
+                  <Linha />
+                  <span>
+                    Sp.Def: {pokemon.stats[4].base_stat}
+                  </span>
+                  <Linha />
+                  <span>
+                    Speed: {pokemon.stats[5].base_stat}
+                  </span>
+                  <Linha />
+                </Colunas>
+              </ContainerColunas>
             </CardPokemonDetalhes>
-           
           </CardPokemon>
         )}
         <CampoDePesquisa>
           <DigitePokemon
             value={pesquisando}
             onChange={(e) => setPesquisando(e.currentTarget.value)}
+            onKeyDown={apertaButao} 
           />
           <ButtonGo onClick={buscarPokemon}>GO!</ButtonGo>
         </CampoDePesquisa>
@@ -92,50 +121,35 @@ const CardPokemon = styled.div`
   
 `;
 const CardPokemonFoto = styled.div`
+  position:relative;
   width: 50%;
-  border-radius: 10px 0px 0px 10px;
+  border-radius: 10px 10px 10px 10px;
   display:flex;
   align-items:center;
   flex-direction:column;
-  img{
+  box-shadow: 5px 5px 5px 1px rgba(0, 0, 0, 0.2);
+`;
+const PokemonFoto = styled.img`
+    position:relative;
     margin-top: 50px;
     position: absolute;
     width:300px;
-    height:300px
-  }
+    height:300px;
 `;
 const CardPokemonDetalhes = styled.div`
-  background-color: yellow;
+  display:flex;
+  flex-direction:column;
   width: 50%;
   border-radius: 0px 10px 10px 0px;
 `;
-const NomeID = styled.div`
-  :root{
-    --bug: #707e59;
-    --dark: #21215a;
-    --dragon: #62cad9;
-    --electric: #eed277;
-    --fairy: #ff7fb2;
-    --fighting: #ef6239;
-    --fire: #d44f5a;
-    --flying: #94b2c7;
-    --ghost: #460d6d;
-    --grass:#70ca86;
-    --ground: #6e491f;
-    --ice: #d8f0fa;
-    --normal: #fff0c0;
-    --poison: rgb(94, 57, 116);
-    --psychic: #cc207c;
-    --rock: #8b3e22;
-    --steel: #60756e;
-    --water:#5d80d1;
-  }
-  background-color:#ef6239;
+
+const NomeID = styled.div`  
+  background-color:#c597c3;
   display:flex;
   flex-direction:row;
   width:100%;
-  height:550px;
-  border-radius: 10px 0px 150px 150px;
+  height:700px;
+  border-radius: 10px 10px 800px 800px;
   justify-content:space-between;
      p{
       color:white;
@@ -147,9 +161,10 @@ const NomeID = styled.div`
       color:white;
       font-family: 'Archivo Black', sans-serif;
       padding:15px;
-      font-size: 50px;
+      font-size: 38px;
      }
 `;
+
 const PesoAltura = styled.div`
   display:flex;
   flex-direction:row;
@@ -159,4 +174,46 @@ const PesoAltura = styled.div`
   justify-content:space-around;
   font-family: Poppins;
   font-weight:500;
+`
+
+const ContainerColunas = styled.div`
+  display:flex;
+  flex-direction: row;
+  width:100%;
+  height:100% ;
+  font-family: Poppins;
+  font-weight:500;
+`
+const Colunas = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items:center;
+  width:50%;
+  height:100% ;
+  font-family: Poppins;
+  font-weight:500;
+
+     p{
+      font-weight:700;
+      font-family: 'Archivo Black', sans-serif;
+      font-size: 20px;
+     }
+     
+     span{
+      padding:41.5px;
+     }
+  `
+const TipoPokemon = styled.div`
+  text-transform: capitalize;
+  background-color:white;
+  display:flex;
+  margin:30px 0px 41.5px;
+  flex-direction: row;
+`
+const Linha = styled.hr`
+  color:#ffffff;
+  background-color: #ffff;
+  height: .5px;
+  width: 150px;
+  border-color: #ffff;
 `
